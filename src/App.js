b4fc52b3a89e45f65c202052b4c7c34840bd08d2
Richard Lirio <richardlirio@hotmail.com>
2024-08-14 18:57:46 -0300

    Criando a sidebar com seus componenter(foto usuario, nome e função) e icone de abrir e fechar o menu lateral.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,18 @@ import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { Routes, Route } from "react-router-dom";
 import Topbar from "./pages/global/Topbar";
-import Dashboard from "./pages/dashboard";
+import Sidebar from "./pages/global/Sidebar";
+import Dashboard from "./pages/dashboard";//dashboard
+// import Time from "./pages/time";//team
+// import Faturas from "./pages/faturas";//Invoices
+// import Contatos from "./pages/contatos";//contacts
+// import Bar from "./pages/bar";
+// import Form from "./pages/form";
+// import Line from "./pages/line";
+// import Pie from "./pages/pie";
+// import FAQ from "./pages/faq";
+// import Geography from "./pages/geography";
+// import Calendario from "./pages/calendario";//calendar
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -12,10 +23,21 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
+          <Sidebar />
           <main className="content">
             <Topbar />
             <Routes>
               <Route path="/" element={<Dashboard />} />
+              {/* <Route path="/time" element={<Time />} /> */}
+              {/* <Route path="/faturas" element={<Faturas />} /> */}
+              {/* <Route path="/contatos" element={<Contatos />} /> */}
+              {/* <Route path="/bar" element={<Bar />} /> */}
+              {/* <Route path="/form" element={<Form />} /> */}
+              {/* <Route path="/line" element={<Line />} /> */}
+              {/* <Route path="/pie" element={<Pie />} /> */}
+              {/* <Route path="/faq" element={<FAQ />} /> */}
+              {/* <Route path="/geografia" element={<Geography />} /> */}
+              {/* <Route path="/calendario" element={<Calendario />} /> */}
             </Routes>
           </main>
         </div>
